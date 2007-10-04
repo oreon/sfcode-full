add7d2eadfa190f2897b9b2f5c9845ac7ac55585
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2007-10-04 20:11:31 +0000

    Initial import.

---
+++ mvnArchetypeJPASpringJSF/trunk/src/main/resources/archetype-resources/src/main/java/JSFDemoBean.java
@@ -0,0 +1,18 @@
+package ${groupId};
+
+import javax.faces.context.FacesContext;
+
+public class JSFDemoBean
+{
+    private String userName = null;
+    
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+    
+    public String getUserName()
+    {
+        return this.userName;
+    }
+}
