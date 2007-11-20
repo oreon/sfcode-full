b8a207d1ce43c2cee70ead8c5f30ab671d0520b4
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2007-11-20 20:02:19 +0000

    Initial import.

---
+++ samples/jshoppingcart/trunk/src/main/java/com/oreon/jshoppingcart/dao/EmployeeDao.java
@@ -0,0 +1,9 @@
+package com.oreon.jshoppingcart.dao;
+
+import com.oreon.jshoppingcart.domain.Employee;
+import org.witchcraft.model.support.dao.GenericDAO;
+import java.util.List;
+
+public interface EmployeeDao extends GenericDAO<Employee> {
+
+}
