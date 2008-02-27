a1bffa941be8beda34e56ee84e196146dfdf5463
codecoaster <codecoaster@38423737-2f20-0410-893e-9c0ab9ae497d>
2008-02-27 10:34:48 +0000

    The message bundle is now working next task to allow switching to another language using the backing bean

--- kgauge/trunk/src/main/java/com/oreon/kgauge/service/LangBean.java
+++ kgauge/trunk/src/main/java/com/oreon/kgauge/service/LangBean.java
@@ -1,24 +1,38 @@
 package com.oreon.kgauge.service;
 
+import java.util.Locale;
+
 import javax.faces.event.ActionEvent;
+import javax.faces.context.FacesContext;
 
 public class LangBean {
 	
-		 private String locale;
-			
-		 public String getLocale() {
+		 private Locale locale=FacesContext.getCurrentInstance().getViewRoot().getLocale();
+		 String localeSt="en";
+		 
+		 
+		 public Locale getLocale() {
 		  return locale;
 		 }
 
 		 public void setLocale(String locale) {
-		  this.locale = locale;
+		  this.localeSt = locale;
 		 }
-			
+		
+		 
+ 
+ 	        public void setLocale(Locale locale) {
+			  	    this.localeSt = locale.getLanguage().substring(0, 2);
+			  	  FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+ 	        }
+
+		 
+		 
 		 public void setLanguage(ActionEvent event){
 		  String id = new String(event.getComponent().getId());
 		  String thislocale=id.substring(12);
 		  System.out.println("event id: "+id+", locale="+thislocale);
-		  setLocale(thislocale);
+		  localeSt=(thislocale);
 		 }
 		}
 
