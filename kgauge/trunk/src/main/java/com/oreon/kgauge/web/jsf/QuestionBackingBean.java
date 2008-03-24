43b5103f7c6a698ccbddcac373f6691bab781446
codecoaster <codecoaster@38423737-2f20-0410-893e-9c0ab9ae497d>
2008-03-24 21:28:33 +0000

    added 2 methods to question backing bean

--- kgauge/trunk/src/main/java/com/oreon/kgauge/web/jsf/QuestionBackingBean.java
+++ kgauge/trunk/src/main/java/com/oreon/kgauge/web/jsf/QuestionBackingBean.java
@@ -6,11 +6,14 @@ import javax.faces.context.FacesContext;
 import org.witchcraft.model.jsf.BaseBackingBean;
 import org.witchcraft.model.support.service.BaseService;
 
+import com.oreon.kgauge.domain.DifficultyLevel;
 import com.oreon.kgauge.domain.Question;
 import com.oreon.kgauge.service.QuestionService;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+
 import org.witchcraft.model.support.Range;
 
 public class QuestionBackingBean extends BaseBackingBean<Question> {
@@ -78,5 +81,26 @@ public class QuestionBackingBean extends BaseBackingBean<Question> {
 		long id = Long.parseLong(component.getValue().toString());
 		 */
 	}
+	//****************Testing***********************************/
+	/** A method to fetch any attributes value from the page*/	
+	private String getValue(String s){
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		Map<String, Object> a=ctx.getViewRoot().getAttributes();
+		String p=(String)a.get(s);
+		return p;}
+	
+	public void setDifficultyLevel() {
+		int dl=Integer.parseInt(getValue("question_difficultyLevel"));
+		switch(dl){
+		case 1: this.question.setDifficultyLevel(DifficultyLevel.L1);break;
+		case 2: this.question.setDifficultyLevel(DifficultyLevel.L2);break;
+		case 3: this.question.setDifficultyLevel(DifficultyLevel.L3);break;
+		case 4: this.question.setDifficultyLevel(DifficultyLevel.L4);break;}}
+	
+	
+	
+	
+	//****************Testing***********************************/
 
 }
+
