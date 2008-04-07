df2e8240910cf2c0595ab8ab62f6c80218e31c43
codecoaster <codecoaster@38423737-2f20-0410-893e-9c0ab9ae497d>
2008-04-07 21:30:48 +0000

    A method for editing questions' text Can be fired on a value change event Not tested yet May be deleted

--- kgauge/trunk/src/main/java/com/oreon/kgauge/domain/Section.java
+++ kgauge/trunk/src/main/java/com/oreon/kgauge/domain/Section.java
@@ -1,8 +1,15 @@
 package com.oreon.kgauge.domain;
 
+import java.util.Iterator;
+
+import javax.faces.event.ActionEvent;
 import javax.persistence.Entity;
 
 import org.apache.log4j.Logger;
+
+import com.oreon.kgauge.domain.Question;
+import javax.faces.model.*;
+import javax.faces.event.*;
 
 @Entity
 public class Section extends SectionBase {
@@ -20,5 +27,19 @@ public class Section extends SectionBase {
 
 	public Section sectionInstance() {
 		return this;
+	}
+	
+	/**Method can be used on Value change event to change a questions text*/
+	public void changeQuestion(ValueChangeEvent vce) {
+		String oldV=(String)vce.getOldValue();
+		String newV=(String)vce.getNewValue();
+		Iterator <Question> qs=super.getQuestionIterator();
+		while(qs.hasNext()){
+			Question q=(Question)qs.next();
+			String qText=q.getQuestionText();
+			if(qText.equals(oldV)){
+				q.setQuestionText(newV);			
+			}
+		}		
 	}
 }
