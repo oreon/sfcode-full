f21dd2fab006918badacdbe738126d9b5f0ea020
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2008-05-21 22:32:23 +0000

    Removed file/folder

--- kgauge/trunk/main/java/com/oreon/kgauge/domain/DifficultyLevel.java
+++
@@ -1,7 +0,0 @@
-package com.oreon.kgauge.domain;
-
-public enum DifficultyLevel {
-
-	L1, L2, L3, L4
-
-}
--- kgauge/trunk/main/java/com/oreon/kgauge/domain/ExamStatus.java
+++
@@ -1,7 +0,0 @@
-package com.oreon.kgauge.domain;
-
-public enum ExamStatus {
-
-	INCOMPLETE, INACTIVE, ACTIVE
-
-}
