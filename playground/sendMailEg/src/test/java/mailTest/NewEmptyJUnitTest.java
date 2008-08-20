a421b53baf76914dfd2085606207267ed13824e6
scjp5 <scjp5@38423737-2f20-0410-893e-9c0ab9ae497d>
2008-08-20 14:08:54 +0000

    This is an example which simulates sending an email from your gmail account. Makes use of an existing gmail account and can be sent to anyone as we will be using Googles smtp server

---
+++ playground/sendMailEg/src/test/java/mailTest/NewEmptyJUnitTest.java
@@ -0,0 +1,44 @@
+package mailTest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+
+import junit.framework.TestCase;
+
+public class NewEmptyJUnitTest extends TestCase {
+
+	ApplicationContext ctx = null;
+    MailSender mailSender = null;
+    SimpleMailMessage mailMessage = null;
+
+    public NewEmptyJUnitTest(String testName) {
+        super(testName);
+        String[] paths = {"mail.xml"};
+        ctx = new ClassPathXmlApplicationContext(paths);
+    }
+	
+    protected void setUp() throws Exception {
+    	mailSender = (MailSender) ctx.getBean("mailSender");
+        mailMessage = (SimpleMailMessage) ctx.getBean("mailMessage");
+	}
+	
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void testSendMail() {
+
+        SimpleMailMessage msg = this.mailMessage;
+        msg.setTo("wcmdauser@gmail.com");
+        msg.setText("This is a test");
+        try {
+            mailSender.send(msg);
+        } catch (MailException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
