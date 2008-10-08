c4305a15f98688f5cf1ff417f99f7107940557f2
scjp5 <scjp5@38423737-2f20-0410-893e-9c0ab9ae497d>
2008-10-08 04:13:02 +0000

    Added functionality to send password along with the mail.

--- kgauge/trunk/src/main/java/com/oreon/kgauge/service/impl/EmailServiceImpl.java
+++ kgauge/trunk/src/main/java/com/oreon/kgauge/service/impl/EmailServiceImpl.java
@@ -5,17 +5,20 @@ import org.springframework.mail.MailSendException;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
 
+import com.oreon.kgauge.domain.Candidate;
+import com.oreon.kgauge.service.CandidateService;
 import com.oreon.kgauge.service.EmailService;
+import com.oreon.kgauge.service.UserService;
 
-public class EmailServiceImpl implements EmailService{
+public class EmailServiceImpl implements EmailService {
 
 	private static final Logger log = Logger.getLogger(EmailServiceImpl.class);
-	
-	
-	private MailSender mailSender ;
+
+	private MailSender mailSender;
 	private SimpleMailMessage mailMessage;
-	
-	
+	private CandidateService candidateService;
+	private UserService userService;
+
 	public void setMailSender(MailSender mailSender) {
 		this.mailSender = mailSender;
 	}
@@ -24,20 +27,34 @@ public class EmailServiceImpl implements EmailService{
 		this.mailMessage = mailMessage;
 	}
 
+	public void setCandidateService(CandidateService candidateService) {
+		this.candidateService = candidateService;
+	}
+
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
 	public void sendEmail(String fromAddress, String toAddress, String subject,
 			String body) throws MailSendException {
-			
+
 	}
-	
-	public void emailPassword(String email) throws MailSendException
-	{ log.debug("Entering email password with mail sender"+this.mailSender +" and " +
-			"mail message " +this.mailMessage +" with email id"+email);
-		
-	this.mailMessage.setTo(email);
+
+	public void emailPassword(String email) throws MailSendException {
+		log.debug("Entering email password with mail sender" + this.mailSender
+				+ " and " + "mail message " + this.mailMessage
+				+ " with email id" + email);
+
+		Candidate candidate = this.candidateService.findByEmail(email);
+
+		String password = candidate.getUser().getPassword();
+
+		this.mailMessage.setText("your password is stored as " + password);
+
+		this.mailMessage.setTo(email);
 		this.mailSender.send(this.mailMessage);
-		
-		log.debug("mail sent successfully to "+email);
+
+		log.debug("mail sent successfully to " + email);
 	}
-	
 
 }
