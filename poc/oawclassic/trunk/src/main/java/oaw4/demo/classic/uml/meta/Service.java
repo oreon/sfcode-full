39b356872918feabdeb143a14a04a5d1911f76c8
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2008-11-30 16:56:07 +0000

    Fixed tests

--- poc/oawclassic/trunk/src/main/java/oaw4/demo/classic/uml/meta/Service.java
+++ poc/oawclassic/trunk/src/main/java/oaw4/demo/classic/uml/meta/Service.java
@@ -1,8 +1,12 @@
 package oaw4.demo.classic.uml.meta;
 
+import java.util.List;
+
+import org.apache.commons.collections.ListUtils;
 import org.openarchitectureware.core.meta.core.ElementSet;
 import org.openarchitectureware.meta.uml.classifier.Class;
 import org.openarchitectureware.meta.uml.classifier.Interface;
+import org.openarchitectureware.meta.uml.classifier.Operation;
 
 /** Reperesents a class containing executable logic
  * @author jsingh
@@ -12,6 +16,28 @@ public class Service extends org.openarchitectureware.meta.uml.classifier.Class{
 
 	public ElementSet getAllAttribs(){
 		return getAttributesForThisClassAndSuperClasses();
+	}
+	
+	/** Will get the operations for all implemented interfaces
+	 * @return
+	 */
+	public ElementSet getOperationsForInterfaces(){
+		List<Interface> interfaces = Interface().toList();
+		ElementSet operations = new ElementSet();
+
+		for (int i = 0; i < interfaces.size(); i++) {
+			operations.addAll(interfaces.get(i).Operation());
+		}
+		
+		return operations;
+	}
+	
+	public ElementSet getAllOperations(){
+		//return ListUtils.sum(getOperationsForInterfaces(), Operation());
+		ElementSet allOps = new ElementSet();
+		allOps.addAll(Operation());
+		allOps.addAll(getOperationsForInterfaces());
+		return allOps;
 		
 	}
 	
@@ -34,6 +60,7 @@ public class Service extends org.openarchitectureware.meta.uml.classifier.Class{
 		
 		return attributes;
 	}
+	
 	
 	public Interface getFirstInterface(){
 		if(!Interface().isEmpty())
