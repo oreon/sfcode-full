5a8b8afc44da11b55c66b51c6a1af2f0d1c9b806
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-01-02 15:19:43 +0000

    added test.java

---
+++ my.generator.project/src/lrnoaw/Test.java
@@ -0,0 +1,12 @@
+package lrnoaw;
+import org.eclipse.uml2.uml.Operation;
+
+
+public class Test {
+	Operation operation;
+	static org.eclipse.uml2.uml.Class cls;
+	
+	public static void main(String[] args) {
+		
+	}
+}
