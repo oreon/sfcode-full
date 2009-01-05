b21f0b0d66599a81994ed9032f9ba373d56635e0
scjp5 <scjp5@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-01-05 09:25:31 +0000

    Skeleton project for use with icefaces + seam and hibernate provided JPA. Please use mvn:package to create the war and deploy in tomcat

---
+++ playground/iceSeamJpa/src/main/java/com/wcmda/model/User.java
@@ -0,0 +1,72 @@
+package com.wcmda.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import static org.jboss.seam.ScopeType.SESSION;
+
+import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
+import org.hibernate.validator.Pattern;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Entity
+@Name("user")
+@Scope(SESSION)
+@Table(name = "Customer")
+public class User implements Serializable {
+
+	private String username;
+	private String password;
+	private String name;
+
+	public User(String username, String password, String name) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.name = name;
+	}
+
+	public User() {
+	}
+
+	@NotNull
+	@Length(max = 100)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@NotNull
+	@Length(min = 5, max = 15)
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Id
+	@Length(min = 5, max = 15)
+	@Pattern(regex = "^\\w*$", message = "not a valid username")
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Override
+	public String toString() {
+		return "User(" + username + ")";
+	}
+}
