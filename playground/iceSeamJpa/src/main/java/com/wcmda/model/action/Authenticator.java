72367753651cedcd410bc1f2b9f94ef73f7b5e68
scjp5 <scjp5@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-03-23 09:03:00 +0000

    changed scope to session

--- playground/iceSeamJpa/src/main/java/com/wcmda/model/action/Authenticator.java
+++ playground/iceSeamJpa/src/main/java/com/wcmda/model/action/Authenticator.java
@@ -9,6 +9,7 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
+import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.bpm.Actor;
 import org.jboss.seam.log.Log;
 import org.jboss.seam.security.Identity;
@@ -17,6 +18,7 @@ import org.jbpm.graph.exe.ProcessInstance;
 import com.wcmda.model.User;
 
 @Name("authenticator")
+@Scope(ScopeType.SESSION)
 public class Authenticator {
 	@Logger
 	Log log;
@@ -30,14 +32,15 @@ public class Authenticator {
 	@In
 	EntityManager entityManager;
 
-	@Out(required = false, scope = ScopeType.SESSION)
+	@Out(required = true, scope = ScopeType.SESSION)
 	User user;
 
 	public boolean authenticate() {
 		// identity.getCredentials().getUsername()
 		// ProcessInstance
 		org.jbpm.graph.exe.ProcessInstance pi = new ProcessInstance();
-
+		
+		
 		Query q = entityManager.createQuery("SELECT u FROM User u "
 				+ "WHERE u.username = #{identity.username} "
 				+ "AND u.password = #{identity.password}");
