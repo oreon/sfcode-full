9544edb5dec9570f770fb3a4f1f391b5520baceb
singhgurpreet <singhgurpreet@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-05-20 18:21:59 +0000

    Updated for image upload

--- imed/src/main/java/org/cerebrum/domain/patient/action/DocumentAction.java
+++ imed/src/main/java/org/cerebrum/domain/patient/action/DocumentAction.java
@@ -1,46 +1,41 @@
 package org.cerebrum.domain.patient.action;
 
-import org.cerebrum.domain.patient.Document;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
-import javax.faces.event.ValueChangeEvent;
-import javax.faces.model.SelectItem;
-import javax.persistence.EntityManager;
+import javax.faces.event.ActionEvent;
 
+import org.cerebrum.domain.patient.Document;
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
-
-import org.apache.commons.lang.StringUtils;
-
 import org.jboss.seam.ScopeType;
-import org.jboss.seam.Component;
-import org.jboss.seam.annotations.Begin;
-import org.jboss.seam.annotations.End;
 import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.In;
-import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Observer;
 import org.jboss.seam.annotations.Out;
 import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.datamodel.DataModel;
 import org.jboss.seam.annotations.datamodel.DataModelSelection;
-import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.log.Log;
-
 import org.witchcraft.seam.action.BaseAction;
-import org.jboss.seam.annotations.Observer;
 
-@Scope(ScopeType.CONVERSATION)
+import com.icesoft.faces.component.inputfile.FileInfo;
+import com.icesoft.faces.component.inputfile.InputFile;
+
+@Scope(ScopeType.SESSION)
 @Name("documentAction")
-public class DocumentAction extends BaseAction<Document>
-		implements
-			java.io.Serializable {
+public class DocumentAction extends BaseAction<Document> implements
+		java.io.Serializable {
 
 	@In(create = true)
 	@Out(required = false)
 	@DataModelSelection
 	private Document document;
+	
+	 private FileInfo currentFile;
 
 	@DataModel
 	private List<Document> documentList;
@@ -65,7 +60,9 @@ public class DocumentAction extends BaseAction<Document>
 		this.documentList = list;
 	}
 
-	/** This function adds associated entities to an example criterion
+	/**
+	 * This function adds associated entities to an example criterion
+	 * 
 	 * @see org.witchcraft.model.support.dao.BaseAction#createExampleCriteria(java.lang.Object)
 	 */
 	public void addAssoications(Criteria criteria) {
@@ -88,4 +85,64 @@ public class DocumentAction extends BaseAction<Document>
 		return documentList;
 	}
 
+	public void uploadFile(ActionEvent event) {
+		  InputFile inputFile = (InputFile) event.getSource();
+	        FileInfo fileInfo = inputFile.getFileInfo();
+	        if (fileInfo.getStatus() == FileInfo.SAVED) {
+	            // reference our newly updated file for display purposes and
+	            // added it to our history file list.
+	            currentFile = fileInfo;
+	        }
+
+	}
+
+	public String save() {
+		try {
+			document.setFile(getBytesFromFile(currentFile));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return super.save();
+		
+	}
+	
+	// Returns the contents of the file in a byte array.
+	private byte[] getBytesFromFile(FileInfo inputFile) throws IOException {
+
+		File file = inputFile.getFile();
+		InputStream inputStream = new FileInputStream(file);
+		// Get the size of the file
+		long length = Long.valueOf(file.length());
+
+		// You cannot create an array using a long type.
+		// It needs to be an int type.
+		// Before converting to an int type, check
+		// to ensure that file is not larger than Integer.MAX_VALUE.
+		if (length > Integer.MAX_VALUE) {
+			// File is too large
+		}
+
+		// Create the byte array to hold the data
+		byte[] bytes = new byte[(int) length];
+
+		// Read in the bytes
+		int offset = 0;
+		int numRead = 0;
+		while (offset < bytes.length
+				&& (numRead = inputStream.read(bytes, offset, bytes.length
+						- offset)) >= 0) {
+			offset += numRead;
+		}
+
+		// Ensure all the bytes have been read in
+		if (offset < bytes.length) {
+			throw new IOException("Could not completely read file "
+					+ file.getName());
+		}
+
+		// Close the input stream and return bytes
+		inputStream.close();
+		return bytes;
+	}
 }
