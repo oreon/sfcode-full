83edf37747dd2d664009b23608992c03182aa1c6
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-09-15 19:21:57 +0000

    Added build number

--- my-webapp/src/main/java/com/ganz/wonders/UtilsClass.java
+++ my-webapp/src/main/java/com/ganz/wonders/UtilsClass.java
@@ -1,9 +1,46 @@
 package com.ganz.wonders;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+import javax.servlet.ServletContext;
+
 public class UtilsClass {
-	
-	public static int add(int a , int b){
-		return a+b;
+
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	public static String getBuildNumber(ServletContext context) {
+
+		String appServerHome = context.getRealPath("/");
+
+		File manifestFile = new File(appServerHome, "META-INF/MANIFEST.MF");
+
+		Manifest mf = new Manifest();
+		try {
+			mf.read(new FileInputStream(manifestFile));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return ("Couldnt read MANIFEST");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Attributes atts = mf.getMainAttributes();
+
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("Version: "
+				+ atts.getValue("Implementation-Version"));
+		buffer.append("Build: " + atts.getValue("Implementation-Build"));
+		
+		return buffer.toString();
+
 	}
 
 }
