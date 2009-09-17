73b02ee1082e197c83e7dece0671df6cd24bd032
spanishphobos <spanishphobos@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-09-17 20:57:13 +0000

    Initial commit

---
+++ pruebasRichfaces/src/main/java/com/witchcraft/domain/MyBean.java
@@ -0,0 +1,16 @@
+package com.witchcraft.domain;
+
+public class MyBean {
+
+	private String text = "domain";
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	
+}
