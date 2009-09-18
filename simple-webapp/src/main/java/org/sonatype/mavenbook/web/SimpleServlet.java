054f3b85be46ea5a6ecc57591360e600df535f56
kalarkar <kalarkar@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-09-18 12:55:17 +0000

    added

--- simple-webapp/src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
+++ simple-webapp/src/main/java/org/sonatype/mavenbook/web/SimpleServlet.java
@@ -9,7 +9,7 @@ public class SimpleServlet extends HttpServlet {
                       HttpServletResponse response)
         throws ServletException, IOException {
 	PrintWriter out = response.getWriter();
-	out.println( "SimpleServlet Executed successfully" );
+	out.println( "SimpleServlet Executed" );
         out.flush();
         out.close();
     }
