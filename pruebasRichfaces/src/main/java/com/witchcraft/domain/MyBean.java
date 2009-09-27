b4c96b641911f90ea2d35c5b243c2eb1aace7bb3
spanishphobos <spanishphobos@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-09-27 09:38:30 +0000

    xml configuration to generate seam, richfaces archetype

--- pruebasRichfaces/src/main/java/com/witchcraft/domain/MyBean.java
+++ pruebasRichfaces/src/main/java/com/witchcraft/domain/MyBean.java
@@ -1,5 +1,11 @@
 package com.witchcraft.domain;
 
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+
+@Name("myBean")
+@Scope(ScopeType.CONVERSATION)
 public class MyBean {
 
 	private String text = "domain";
