7db4a5fff5a6a7324cddc5b37ccc51df6e412315
spanishphobos <spanishphobos@38423737-2f20-0410-893e-9c0ab9ae497d>
2009-11-28 18:36:36 +0000

    1.- Change scope of javax.servlet from runtime to provided, because it's necesary to compile but no in war file.
    2.- Add conversation scope of components AccountList, CustomerList and ProductList
    
    ================
    
    1.- Cambiamos el scope de javax.servlet de runtime a provided, porque es necesaria para compilar pero no en el fichero war.
    
    2.- Añadimos al ámbito de conversación los componentes AccountList, CustomerList y ProductList

--- newshopper/src/main/java/com/shan/customermgt/domain/action/AccountList.java
+++ newshopper/src/main/java/com/shan/customermgt/domain/action/AccountList.java
@@ -2,15 +2,23 @@ package com.shan.customermgt.domain.action;
 
 import java.util.Arrays;
 
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.framework.EntityQuery;
 
 import com.shan.customermgt.domain.Account;
 
 @Name("accountList")
+@Scope(ScopeType.CONVERSATION)
 public class AccountList extends EntityQuery<Account> {
 
-	private static final String EJBQL = "select account from Account account";
+	/**
+     * 
+     */
+    private static final long serialVersionUID = -1969725967850006227L;
+
+    private static final String EJBQL = "select account from Account account";
 
 	private static final String[] RESTRICTIONS = {
 		"account.customer.id == #{accountList.account.customer.id}",
--- newshopper/src/main/java/com/shan/customermgt/domain/action/ProductList.java
+++ newshopper/src/main/java/com/shan/customermgt/domain/action/ProductList.java
@@ -2,15 +2,23 @@ package com.shan.customermgt.domain.action;
 
 import java.util.Arrays;
 
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.framework.EntityQuery;
 
 import com.shan.customermgt.domain.Product;
 
 @Name("productList")
+@Scope(ScopeType.CONVERSATION)
 public class ProductList extends EntityQuery<Product> {
 
-	private static final String EJBQL = "select product from Product product";
+	/**
+     * 
+     */
+    private static final long serialVersionUID = -2197823398395318313L;
+
+    private static final String EJBQL = "select product from Product product";
 
 	private static final String[] RESTRICTIONS = {"lower(product.name) like concat(lower(#{productList.product.name}),'%')",};
 
