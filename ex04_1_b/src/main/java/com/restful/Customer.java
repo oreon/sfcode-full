39650a3df0546364bb1404f074fae243bd178a0d
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2010-04-29 02:03:05 +0000

    adding the source files

---
+++ ex04_1_b/src/main/java/com/restful/Customer.java
@@ -0,0 +1,84 @@
+package com.restful;
+public class Customer {
+
+	private int id;
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	private String firstName;
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	private String lastName;
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	private String street;
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	private String city;
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	private String state;
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	private String zip;
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	private String country;
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+}
---
+++ ex04_1_b/src/main/java/com/restful/CustomerImpl.java
@@ -0,0 +1,4 @@
+package com.restful;
+public class CustomerImpl {
+
+}
