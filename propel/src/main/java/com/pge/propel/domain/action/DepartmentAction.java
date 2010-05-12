9f1cef0f9da5303f45470f6e22a517c776624521
singhgurpreet <singhgurpreet@38423737-2f20-0410-893e-9c0ab9ae497d>
2010-05-12 13:37:22 +0000

    updated for fulltext search

--- propel/src/main/java/com/pge/propel/domain/action/DepartmentAction.java
+++ propel/src/main/java/com/pge/propel/domain/action/DepartmentAction.java
@@ -1,6 +1,4 @@
-
 package com.pge.propel.domain.action;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,8 +9,13 @@ import javax.persistence.EntityManager;
 
 import org.hibernate.Criteria;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.search.jpa.FullTextQuery;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.lucene.analysis.StopAnalyzer;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Query;
 
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Scope;
@@ -30,9 +33,29 @@ import org.jboss.seam.annotations.datamodel.DataModelSelection;
 import org.jboss.seam.faces.FacesMessages;
 import org.jboss.seam.log.Log;
 import org.jboss.seam.annotations.Observer;
-	
+
+import com.pge.propel.domain.Department;
+
 @Scope(ScopeType.CONVERSATION)
 @Name("departmentAction")
-	public class DepartmentAction extends DepartmentActionBase implements java.io.Serializable{
-	
+public class DepartmentAction extends DepartmentActionBase implements
+		java.io.Serializable {
+
+	public List<Department> autoComplete(Object o) {
+
+		QueryParser parser = new QueryParser("name", new StopAnalyzer());
+		Query luceneQuery = null;
+		try {
+			luceneQuery = parser.parse("name:" + o.toString());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FullTextQuery query = this.getEntityManager().createFullTextQuery(
+				luceneQuery, Department.class);
+		// String sql =
+		// "select from com.pge.propel.domain.Employee e where e.firstName like '%"+o.toString()+"%')";
+		return query.getResultList();// this.getEntityManager().createQuery("select emp from com.pge.propel.domain.Employee emp").getResultList();
 	}
+
+}
