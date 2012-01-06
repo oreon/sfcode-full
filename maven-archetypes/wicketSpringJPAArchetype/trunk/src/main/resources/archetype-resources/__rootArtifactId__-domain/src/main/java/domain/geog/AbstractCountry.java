9ae011737dd79e51ee846fbb97a587be9b5971cd
kpsingh <kpsingh@38423737-2f20-0410-893e-9c0ab9ae497d>
2012-01-06 17:50:43 +0000

    File Renamed and code refactored

---
+++ maven-archetypes/wicketSpringJPAArchetype/trunk/src/main/resources/archetype-resources/__rootArtifactId__-domain/src/main/java/domain/geog/AbstractCountry.java
@@ -0,0 +1,103 @@
+package ${package}.domain.geog;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.TableGenerator;
+
+import ${package}.domain.BaseEntity;
+
+/**
+ * @author Kamalpreet Singh
+ *
+ */
+@MappedSuperclass
+public abstract class AbstractCountry extends BaseEntity {
+
+	/** Serialization version UID. */
+	private static final long serialVersionUID = 8695800485009228471L;
+	
+	/** The primary key of the Country. */
+	@Id
+	@TableGenerator(name = "COUNTRY_ID_GENERATOR", table = "PRIMARY_KEY_GENERATOR", pkColumnName = "GENERATOR_KEY", 
+			valueColumnName = "GENERATOR_VALUE", pkColumnValue = "COUNTRY_ID")
+	@GeneratedValue(strategy = javax.persistence.GenerationType.TABLE, generator = "COUNTRY_ID_GENERATOR")
+	@Column(name = "COUNTRY_ID", nullable = false, updatable = false)
+	private Long id;
+
+	/** Flag that tells whether a Country is active or not. */
+	@Column(name = "ACTIVE", length = 1, nullable = false)
+	private boolean active;
+	
+	/** The code of the Country. */
+	@Column(name = "CODE", length = 50, nullable = false, unique = true)
+	private String code;
+	
+	/** The name of the Country. */
+	@Column(name = "NAME", length = 100, nullable = false)
+	private String name;
+	
+	/**
+	 * Default no-argument constructor.
+	 */
+	public AbstractCountry() {
+		
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the active
+	 */
+	public boolean isActive() {
+		return active;
+	}
+
+	/**
+	 * @param active the active to set
+	 */
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
