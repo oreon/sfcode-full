187e3f773aff560f1fb2795abaed2633dddeb901
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2014-02-08 00:05:07 +0000

    Updated the button for editing

--- cerebrumehr/src/main/java/com/oreon/cerebrum/web/action/prescription/PrescriptionListQuery.java
+++ cerebrumehr/src/main/java/com/oreon/cerebrum/web/action/prescription/PrescriptionListQuery.java
@@ -1,15 +1,37 @@
-
 package com.oreon.cerebrum.web.action.prescription;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
-
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
-	
-	
-@Name("prescriptionList")
-//@Scope(ScopeType.CONVERSATION)
-//@Restrict("#{s:hasRole('Physician')}")
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.framework.EntityQuery;
 
-public class PrescriptionListQuery extends PrescriptionListQueryBase implements java.io.Serializable{
-	
+import org.witchcraft.seam.action.BaseQuery;
+
+import org.witchcraft.base.entity.Range;
+
+import org.primefaces.model.SortOrder;
+import org.witchcraft.seam.action.EntityLazyDataModel;
+import org.primefaces.model.LazyDataModel;
+import java.util.Map;
+
+import org.jboss.seam.annotations.Observer;
+
+import java.math.BigDecimal;
+import javax.faces.model.DataModel;
+
+import org.jboss.seam.annotations.security.Restrict;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.Component;
+
+@Name("prescriptionList")
+@Scope(ScopeType.CONVERSATION)
+public class PrescriptionListQuery extends PrescriptionListQueryBase
+		implements
+			java.io.Serializable {
+
 }
