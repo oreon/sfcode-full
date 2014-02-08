6db0c4a8ec5ac0c9fecbfce57138222522b7b520
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2014-02-08 00:05:07 +0000

    Updated the button for editing

--- cerebrumehr/src/main/java/com/oreon/cerebrum/web/action/ddx/FindingListQuery.java
+++ cerebrumehr/src/main/java/com/oreon/cerebrum/web/action/ddx/FindingListQuery.java
@@ -1,28 +1,37 @@
-
 package com.oreon.cerebrum.web.action.ddx;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
-
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
-	
-	
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.framework.EntityQuery;
+
+import org.witchcraft.seam.action.BaseQuery;
+
+import org.witchcraft.base.entity.Range;
+
+import org.primefaces.model.SortOrder;
+import org.witchcraft.seam.action.EntityLazyDataModel;
+import org.primefaces.model.LazyDataModel;
+import java.util.Map;
+
+import org.jboss.seam.annotations.Observer;
+
+import java.math.BigDecimal;
+import javax.faces.model.DataModel;
+
+import org.jboss.seam.annotations.security.Restrict;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.Component;
+
 @Name("findingList")
-//@Scope(ScopeType.CONVERSATION)
-public class FindingListQuery extends FindingListQueryBase implements java.io.Serializable{
-	
-	private static final String[] CUSTOM_RESTRICTIONS = {
-		"finding.id = #{findingList.finding.id}",
+@Scope(ScopeType.CONVERSATION)
+public class FindingListQuery extends FindingListQueryBase
+		implements
+			java.io.Serializable {
 
-		"lower(finding.name) like concat('%',lower(#{findingList.finding.name}),'%')",
-
-		"finding.dateCreated <= #{findingList.dateCreatedRange.end}",
-		"finding.dateCreated >= #{findingList.dateCreatedRange.begin}",};
-	
-	
-	@Override
-	public String[] getEntityRestrictions() {
-		// TODO Auto-generated method stub
-		return CUSTOM_RESTRICTIONS;
-	}
-	
 }
--- cerebrumehr/src/main/java/com/oreon/cerebrum/web/action/drugs/DrugListQuery.java
+++ cerebrumehr/src/main/java/com/oreon/cerebrum/web/action/drugs/DrugListQuery.java
@@ -1,17 +1,37 @@
-
 package com.oreon.cerebrum.web.action.drugs;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
-
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
-	
-	
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.framework.EntityQuery;
+
+import org.witchcraft.seam.action.BaseQuery;
+
+import org.witchcraft.base.entity.Range;
+
+import org.primefaces.model.SortOrder;
+import org.witchcraft.seam.action.EntityLazyDataModel;
+import org.primefaces.model.LazyDataModel;
+import java.util.Map;
+
+import org.jboss.seam.annotations.Observer;
+
+import java.math.BigDecimal;
+import javax.faces.model.DataModel;
+
+import org.jboss.seam.annotations.security.Restrict;
+
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.Component;
+
 @Name("drugList")
-//@Scope(ScopeType.CONVERSATION)
-public class DrugListQuery extends DrugListQueryBase implements java.io.Serializable{
-	
-	public DrugListQuery() {
-		setOrder("drug.name");
-	}
-	
+@Scope(ScopeType.CONVERSATION)
+public class DrugListQuery extends DrugListQueryBase
+		implements
+			java.io.Serializable {
+
 }
