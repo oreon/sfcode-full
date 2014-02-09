6ec09d7e1688690731ac5b38dd54f26adf8939b9
shadeven <shadeven@38423737-2f20-0410-893e-9c0ab9ae497d>
2014-02-09 13:18:38 +0000

    Modal dialog issue investigation(Work in progress).

---
+++ primeseam/src/main/webapp/js/commons.js
@@ -0,0 +1,37 @@
+function handleDialogSubmit(xhr, status, args) {
+	if (args != null) {
+		if (args.validationFailed) {
+			editProductDialog.show();
+		} else {
+			editProductDialog.hide();
+		}
+	}
+}
+
+function processObjectsChange(suggestionBox) {
+    var id = suggestionBox.oldValue;
+    this.location.href = location + "?id=" + id;
+}
+
+
+function processObjectsChangeLoc(suggestionBox, loc) {
+	//alert(loc);
+    var id = suggestionBox.oldValue;
+    this.location.href = loc + "?id=" + id;
+}
+
+
+function processObjectsChangeLocWithParamName(suggestionBox, loc, name) {
+	//alert(loc);
+    var id = suggestionBox.oldValue;
+    this.location.href = loc + "?" + name + "=" + id;
+}
+
+function processSearch( loc, id, name) {
+	alert(loc + " " + id + " " +name);
+	
+    var id = suggestionBox.oldValue;
+    this.location.href = loc + "?" + name + "=" + id;
+}
+
+
