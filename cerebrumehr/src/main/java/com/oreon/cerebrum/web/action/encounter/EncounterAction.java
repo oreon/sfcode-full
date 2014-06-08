165454d5f9ad68c907157be1c82f06bf9463fec1
singhj <singhj@38423737-2f20-0410-893e-9c0ab9ae497d>
2014-06-08 11:12:32 +0000

    Enabled models and added PF extensions

--- cerebrumehr/src/main/java/com/oreon/cerebrum/web/action/encounter/EncounterAction.java
+++ cerebrumehr/src/main/java/com/oreon/cerebrum/web/action/encounter/EncounterAction.java
@@ -1,65 +1,63 @@
-
-	
 package com.oreon.cerebrum.web.action.encounter;
-	
 
 import java.util.ArrayList;
 import java.util.List;
 
-import org.jboss.seam.Component;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
-import org.jboss.seam.security.Identity;
 import org.witchcraft.base.entity.UserUtilAction;
 import org.witchcraft.exceptions.BusinessException;
 
 import com.oreon.cerebrum.codes.SimpleCode;
+import com.oreon.cerebrum.prescription.Prescription;
 import com.oreon.cerebrum.web.action.employee.PhysicianAction;
 
-	
 //@Scope(ScopeType.CONVERSATION)
 @Name("encounterAction")
-public class EncounterAction extends EncounterActionBase implements java.io.Serializable{
-	
-	@In(create=true)
+public class EncounterAction extends EncounterActionBase implements
+		java.io.Serializable {
+
+	@In(create = true)
 	PhysicianAction physicianAction;
-	
-	@In(create=true)
+
+	@In(create = true)
 	UserUtilAction userUtilAction;
-	
+
 	private List<SimpleCode> selectedSimpleCodes = new ArrayList<SimpleCode>();
-	
-	public EncounterAction(){
+
+	public EncounterAction() {
 		super();
 		/*
-		Encounter encounter = getInstance();
-		if(isNew() && encounter.getListReasons().isEmpty()){
-			encounter.getListReasons().add(new Reason());
-		}
-		*/
+		 * Encounter encounter = getInstance(); if(isNew() &&
+		 * encounter.getListReasons().isEmpty()){
+		 * encounter.getListReasons().add(new Reason()); }
+		 */
 	}
-	
+
 	@Override
 	public String save(boolean endConversation) {
 		instance.setCreator(userUtilAction.getCurrentLoggedInEmployee());
-		
+
 		if (getInstance().getPatient() == null) {
-			if(patientAction.getInstance() == null || patientAction.getInstance().getId() == null)
+			if (patientAction.getInstance() == null
+					|| patientAction.getInstance().getId() == null)
 				throw new BusinessException("Must Select a patient");
 			getInstance().setPatient(patientAction.getInstance());
 		}
-		
-		
-		prescriptionAction.getInstance().setPatient(instance.getPatient());
-		prescriptionAction.save();
-		
+
+		Prescription prescription = prescriptionAction.getInstance();
+		if (!prescription.getListPrescriptionItems().isEmpty()) {
+			prescription.setPatient(instance.getPatient());
+			prescriptionAction.save();
+			instance.setPrescription(prescription);
+		}
+
 		/*
-		patientAction.setInstance(instance.getPatient()); 	
-		patientAction.save(endConversation);
-		*/
-		
-		instance.setPrescription(prescriptionAction.getInstance());
-		//getInstance().setCreatedByUser(userUtilAction.getCurrentUser());
+		 * patientAction.setInstance(instance.getPatient());
+		 * patientAction.save(endConversation);
+		 */
+
+		// getInstance().setCreatedByUser(userUtilAction.getCurrentUser());
 		return super.save(endConversation);
 	}
 
@@ -70,6 +68,5 @@ public class EncounterAction extends EncounterActionBase implements java.io.Seri
 	public List<SimpleCode> getSelectedSimpleCodes() {
 		return selectedSimpleCodes;
 	}
-	
+
 }
-	
